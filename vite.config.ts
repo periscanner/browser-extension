d67e2af3c7576315e294072f83a40c587232911c
LuisPacx <unknown.pluis@gmail.com>
2026-01-16 14:33:30 -0300

    chore: use new lib to zip release

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,19 @@ import path from 'node:path'
 import { crx } from '@crxjs/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import zip from 'vite-plugin-zip-pack'
 import manifest from './manifest.config.ts'
-import { name, version } from './package.json'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': `${path.resolve(__dirname, 'src')}`,
     },
   },
   plugins: [
     vue(),
     crx({ manifest }),
     tailwindcss(),
-    zip({
-      inDir: path.resolve(__dirname, 'dist'), // Absolute path to dist
-      outDir: path.resolve(__dirname, 'release'), // Absolute path to release
-      outFileName: `crx-${name}-${version}.zip`,
-      pathPrefix: ''
-    }),
   ],
   server: {
     cors: {
@@ -31,7 +23,4 @@ export default defineConfig({
       ],
     },
   },
-  build: {
-    emptyOutDir: true, // Ensures a clean dist before zipping
-  }
 })
